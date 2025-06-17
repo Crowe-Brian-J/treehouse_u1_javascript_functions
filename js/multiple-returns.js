5bb3ad0b93f61538c57acde8b814eb791a7369e6
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-17 15:49:46 -0400

    multiple-returns.js and index.html updates - cannot return multiple values

---
+++ js/multiple-returns.js
@@ -0,0 +1,14 @@
+const isFieldEmpty = () => {
+  const field = document.querySelector('#info')
+  if (field.value === '') {
+    return true
+  } else {
+    return false
+  }
+}
+
+const fieldTest = isFieldEmpty()
+
+if (fieldTest === true) {
+  alert('Please input')
+}
