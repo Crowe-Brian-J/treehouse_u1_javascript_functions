91d0ce3dc6a48a7dd98e2b66a6b1177965261012
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-17 16:31:16 -0400

    variable scope exercise

--- js/scope.js
+++ js/scope.js
@@ -1,4 +1,18 @@
+let person = 'Lee'
+
 function greeting() {
   let person = 'Meg'
+  //person = 'Meg' --> used to show that person is stored to Meg across scope.js
   alert(`Hi, ${person}!`)
 }
+
+function greeting2() {
+  let person = 'Robert'
+  alert(`Good morning, ${person}!`)
+}
+
+greeting()
+alert(`Hi, ${person}!`)
+greeting2()
+
+// ^ Done to show that functions use variable declared inside function
