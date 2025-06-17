b063b4b2d6eb85097abfe1030352cfb52e7ed46c
Brian Crowe <brian.j.crowe@protonmail.com>
2025-06-17 16:40:35 -0400

    function declarations vs function expressions, scope and readability

--- js/expressions.js
+++ js/expressions.js
@@ -1,4 +1,20 @@
+/*
+function declaration vs function expressions --> function to const
+declarations can be defined afer they are called
+expressions need to be defined before they are called (out of scope)
+general rule of thumb is place functions at the top of the code for function and readability
+*/
+
+/*
 function getRandomNumber(upper) {
+  const randomNumber = Math.floor(Math.random() * upper ) + 1;
+  return randomNumber;
+}
+*/
+
+const getRandomNumber = function (upper) {
   const randomNumber = Math.floor(Math.random() * upper) + 1
   return randomNumber
 }
+
+getRandomNumber(10)
